79f523012f6b4085607742f2b78f11546d5e3bd1
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-04-22 01:09:51 +0200

    Project Setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="text-2xl text-red-500">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque cupiditate
+    qui id excepturi porro enim suscipit ipsam adipisci dolor expedita
+    consequatur quisquam alias molestiae maiores, praesentium odio nobis?
+    Aperiam, asperiores?
+  </div>
+</template>
+
+<style scoped></style>

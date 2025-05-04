6e492185e3db6ef8bd7e4a4c13fb714082cc81a0
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-04 03:14:46 +0300

    add initial dashboard

--- src/pages/Dashboard.page.vue
+++ src/pages/Dashboard.page.vue
@@ -1,5 +1,31 @@
-<template></template>
+<script setup lang="ts">
+import EcommerceMetrics from '../components/ecommerce/EcommerceMetrics.vue';
+import MonthlyTarget from '../components/ecommerce/MonthlyTarget.vue';
+import MonthlySale from '../components/ecommerce/MonthlySale.vue';
+import StatisticsChart from '../components/ecommerce/StatisticsChart.vue';
+import CustomerDemographic from '../components/ecommerce/CustomerDemographic.vue';
+import RecentOrders from '../components/ecommerce/RecentOrders.vue';
+</script>
+<template>
+  <div class="grid grid-cols-12 gap-4 md:gap-6">
+    <div class="col-span-12 space-y-6 xl:col-span-7">
+      <ecommerce-metrics />
+      <monthly-sale />
+    </div>
+    <div class="col-span-12 xl:col-span-5">
+      <monthly-target />
+    </div>
 
-<script setup lang="ts"></script>
+    <div class="col-span-12">
+      <statistics-chart />
+    </div>
 
-<style scoped></style>
+    <div class="col-span-12 xl:col-span-5">
+      <customer-demographic />
+    </div>
+
+    <div class="col-span-12 xl:col-span-7">
+      <recent-orders />
+    </div>
+  </div>
+</template>

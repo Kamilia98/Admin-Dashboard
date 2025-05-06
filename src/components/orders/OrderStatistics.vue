bfb8163d4e5d9e6cb8389e7eb221c0565898a2a3
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-06 03:20:28 +0300

    make common card

--- src/components/orders/OrderStatistics.vue
+++ src/components/orders/OrderStatistics.vue
@@ -2,6 +2,7 @@
 import { computed, onMounted } from 'vue';
 import { BoxCubeIcon } from '../../icons';
 import { useOrdersStore } from '../../stores/orderStore';
+import Card from '../common/Card.vue';
 
 const store = useOrdersStore();
 
@@ -18,79 +19,16 @@ const averageOrderValue = computed(() => {
 
 <template>
   <div class="grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-3 md:gap-6">
-    <!-- Orders Analytics Card -->
-    <div
-      class="rounded-2xl border border-gray-200 bg-white p-5 md:p-6 dark:border-gray-800 dark:bg-white/[0.03]"
-    >
-      <div
-        class="flex h-12 w-12 items-center justify-center rounded-xl bg-gray-100 dark:bg-gray-800 dark:text-gray-400"
-      >
-        <BoxCubeIcon />
-      </div>
-
-      <div class="mt-5 flex items-end justify-between">
-        <div>
-          <span class="text-sm text-gray-500 dark:text-gray-400">Orders</span>
-          <h4
-            class="mt-2 text-title-sm font-bold text-gray-800 dark:text-white/90"
-          >
-            {{ store.totalOrders }}
-          </h4>
-        </div>
-        <!-- <span
-          class="flex items-center gap-1 rounded-full bg-error-50 py-0.5 pr-2.5 pl-2 text-sm font-medium text-error-600 dark:bg-error-500/15 dark:text-error-500"
-        >
-          <el-icon><Bottom /></el-icon> 9.05%
-        </span> -->
-      </div>
-    </div>
-
-    <!-- Total Revenue Analytics Card -->
-    <div
-      class="rounded-2xl border border-gray-200 bg-white p-5 md:p-6 dark:border-gray-800 dark:bg-white/[0.03]"
-    >
-      <div
-        class="flex h-12 w-12 items-center justify-center rounded-xl bg-gray-100 dark:bg-gray-800 dark:text-gray-400"
-      >
-        <BoxCubeIcon />
-      </div>
-
-      <div class="mt-5 flex items-end justify-between">
-        <div>
-          <span class="text-sm text-gray-500 dark:text-gray-400"
-            >Total Revenue</span
-          >
-          <h4
-            class="mt-2 text-title-sm font-bold text-gray-800 dark:text-white/90"
-          >
-            ${{ store.totalRevenue.toFixed(2) }}
-          </h4>
-        </div>
-      </div>
-    </div>
-
-    <!-- Average Order Value Analytics Card -->
-    <div
-      class="rounded-2xl border border-gray-200 bg-white p-5 md:p-6 dark:border-gray-800 dark:bg-white/[0.03]"
-    >
-      <div
-        class="flex h-12 w-12 items-center justify-center rounded-xl bg-gray-100 dark:bg-gray-800 dark:text-gray-400"
-      >
-        <BoxCubeIcon />
-      </div>
-
-      <div class="mt-5 flex items-end justify-between">
-        <div>
-          <span class="text-sm text-gray-500 dark:text-gray-400"
-            >Average Order Value</span
-          >
-          <h4
-            class="mt-2 text-title-sm font-bold text-gray-800 dark:text-white/90"
-          >
-            ${{ averageOrderValue }}
-          </h4>
-        </div>
-      </div>
-    </div>
+    <Card title="Orders" :value="store.totalOrders" :icon="BoxCubeIcon" />
+    <Card
+      title="Total Revenue"
+      :value="`$${store.totalRevenue.toFixed(2)}`"
+      :icon="BoxCubeIcon"
+    />
+    <Card
+      title="Average Order Value"
+      :value="`$${averageOrderValue}`"
+      :icon="BoxCubeIcon"
+    />
   </div>
 </template>

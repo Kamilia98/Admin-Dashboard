6416858851ca3030d05e70bf954d12d0dd1745f7
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-07 06:51:02 +0300

    added filled button type

--- src/components/common/Button.vue
+++ src/components/common/Button.vue
@@ -6,7 +6,7 @@
       variantClasses,
     ]"
     @click="$emit('click')"
-    v-bind="tag === 'router-link' ? { to } : {}"
+    v-bind="tag === 'router-link' ? { to: to || null } : {}"
   >
     <!-- Icon Slot -->
     <slot name="icon" />
@@ -25,15 +25,9 @@ const props = defineProps({
     validator: (value: string) => ['button', 'router-link'].includes(value),
   },
   to: {
-    type: Object,
-    default: '',
-    validator: (value: unknown) => {
-      try {
-        return typeof value === 'string' || typeof value === 'object';
-      } catch {
-        return false;
-      }
-    },
+    type: [String, Object], // Accepting both string or object for 'to'
+    default: null, // Default to null
+    required: false,
   },
   variant: {
     type: String,
@@ -41,18 +35,39 @@ const props = defineProps({
     validator: (value: string) =>
       ['default', 'primary', 'danger'].includes(value),
   },
+  type: {
+    type: String,
+    default: 'plain',
+    validator: (value: string) => ['plain', 'filled'].includes(value),
+  },
 });
 
 defineEmits(['click']);
 
 const variantClasses = computed(() => {
-  switch (props.variant) {
-    case 'primary':
-      return 'text-brand-500 border border-brand-500 hover:bg-brand-100 dark:text-brand-500 dark:bg-gray-800 dark:hover:bg-brand-900/10 dark:border-gray-700';
-    case 'danger':
-      return 'text-error-500 border border-error-500 hover:bg-error-100 dark:text-error-500 dark:bg-gray-800 dark:bg-gray-800 dark:hover:bg-error-900/10 dark:border-gray-700';
-    default:
-      return 'border border-gray-300 bg-white text-gray-700 hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3 dark:hover:text-gray-200';
-  }
+  const base = {
+    default: {
+      plain:
+        'border border-gray-300 bg-white text-gray-700 hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/3 dark:hover:text-gray-200',
+      filled:
+        'bg-gray-700 text-white hover:bg-gray-800 dark:bg-gray-600 dark:hover:bg-gray-500',
+    },
+    primary: {
+      plain:
+        'text-brand-500 border border-brand-500 hover:bg-brand-100 dark:text-brand-500 dark:bg-gray-800 dark:hover:bg-brand-900/10 dark:border-gray-700',
+      filled:
+        'bg-brand-500 text-white hover:bg-brand-600 dark:bg-brand-600 dark:hover:bg-brand-500',
+    },
+    danger: {
+      plain:
+        'text-error-500 border border-error-500 hover:bg-error-100 dark:text-error-500 dark:bg-gray-800 dark:hover:bg-error-900/10 dark:border-gray-700',
+      filled:
+        'bg-error-500 text-white hover:bg-error-600 dark:bg-error-600 dark:hover:bg-error-500',
+    },
+  };
+
+  return base[props.variant as keyof typeof base][
+    props.type as keyof typeof base.default
+  ];
 });
 </script>

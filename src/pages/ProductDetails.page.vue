90e39ff9c9dc09c938e2a1908e7bfda16613a0e8
Mohamedabdullhaleam <mohamedabdullhaleam236@gmail.com>
2025-05-10 05:46:03 +0300

    refactor: clean up code and improve styling for product details page

--- src/pages/ProductDetails.page.vue
+++ src/pages/ProductDetails.page.vue
@@ -7,35 +7,30 @@ const route = useRoute();
 const productStore = useProductStore();
 
 const product = ref<Product | null>(null);
-const selectedColor = ref<Color | null>(null); // Selected color state
-const selectedImage = ref<string | null>(null); // Image for the selected color
+const selectedColor = ref<Color | null>(null);
+const selectedImage = ref<string | null>(null);
 
-// Fetch the product details when the component is mounted
 onMounted(async () => {
   const productId = route.params.id as string;
   console.log('[Product-details --- ID]', productId);
   product.value = await productStore.getProductById(productId);
   console.log('[Product-details --- Product Details]', product.value);
 
-  // Set the initial selected color and image (first color and its corresponding image)
   if (product.value?.colors && product.value.colors.length > 0) {
     selectedColor.value = product.value.colors[0];
-    selectedImage.value = product.value.colors[0]?.images[0]?.url || ''; // Use the URL field here
+    selectedImage.value = product.value.colors[0]?.images[0]?.url || '';
   }
 });
 
-// Function to handle color selection and update the displayed image
 function selectColor(color: Color) {
   selectedColor.value = color;
-  selectedImage.value = color.images[0]?.url || ''; // Use the URL field here
+  selectedImage.value = color.images[0]?.url || '';
 }
 
-// Function to handle image selection
 function selectImage(imageUrl: string) {
-  selectedImage.value = imageUrl; // Update the selected image
+  selectedImage.value = imageUrl;
 }
 
-// Helper function to get the index of the selected image
 function getImageIndex() {
   return (
     selectedColor.value?.images.findIndex(
@@ -43,23 +38,6 @@ function getImageIndex() {
     ) ?? -1
   );
 }
-
-// function goToPreviousImage() {
-//   const currentIndex = getImageIndex();
-//   if (currentIndex > 0) {
-//     selectedImage.value =
-//       selectedColor.value?.images[currentIndex - 1].url || '';
-//   }
-// }
-
-// // Function to go to the next image
-// function goToNextImage() {
-//   const currentIndex = getImageIndex();
-//   if (currentIndex < (selectedColor.value?.images.length ?? 0) - 1) {
-//     selectedImage.value =
-//       selectedColor.value?.images[currentIndex + 1].url || '';
-//   }
-// }
 </script>
 
 <template>
@@ -68,26 +46,26 @@ function getImageIndex() {
       <!-- Left Section: Image Gallery -->
       <div class="space-y-6">
         <!-- Main Image -->
-        <div class="aspect-square rounded-xl bg-gray-50 p-8">
+        <div class="aspect-square rounded-lg bg-gray-50">
           <img
             :src="selectedImage"
             alt="Selected product image"
-            class="h-full w-full object-contain object-center"
+            class="h-full w-full rounded-lg object-cover"
           />
         </div>
 
         <!-- Thumbnails Grid -->
-        <div class="grid grid-cols-5 gap-4">
+        <div class="grid grid-cols-6 gap-4">
           <div
             v-for="img in selectedColor?.images"
             :key="img._id"
             @click="selectImage(img.url)"
-            class="aspect-square cursor-pointer rounded-lg bg-gray-50 transition-all hover:opacity-90"
+            class="aspect-square cursor-pointer rounded-lg bg-gray-50 object-cover transition-all hover:opacity-90"
             :class="{
-              'ring-2 ring-brand-500': selectedImage === img.url,
+              'ring-1 ring-brand-500': selectedImage === img.url,
             }"
           >
-            <img :src="img.url" class="h-full w-full object-contain p-2" />
+            <img :src="img.url" class="h-full w-full rounded-lg object-cover" />
           </div>
         </div>
       </div>
@@ -96,7 +74,9 @@ function getImageIndex() {
       <div class="space-y-8">
         <!-- Header -->
         <div class="space-y-4">
-          <h1 class="text-4xl font-light text-gray-900">{{ product.name }}</h1>
+          <h1 class="text-4xl font-light text-gray-900 dark:text-gray-100">
+            {{ product.name }}
+          </h1>
           <p class="text-lg text-gray-500">{{ product.subtitle }}</p>
           <div class="flex items-baseline gap-4">
             <span class="text-3xl font-medium text-brand-500">
@@ -114,33 +94,38 @@ function getImageIndex() {
 
         <!-- Color Selection -->
         <div class="space-y-4">
-          <h3 class="text-sm font-medium text-gray-700">COLOR OPTIONS</h3>
+          <h3 class="font-medium text-gray-700 dark:text-gray-300">
+            COLOR OPTIONS
+          </h3>
           <div class="flex flex-wrap gap-3">
             <div
               v-for="color in product.colors"
               :key="color._id"
               :style="{ backgroundColor: color.hex }"
               class="h-12 w-12 cursor-pointer rounded-full shadow-sm transition-all hover:scale-105"
-              :class="{
-                'ring-2 ring-brand-500 ring-offset-2':
-                  selectedColor?._id === color._id,
-              }"
+              :class="[
+                selectedColor?._id === color._id
+                  ? 'ring-2 ring-brand-500 ring-offset-2'
+                  : 'ring-1 ring-white dark:ring-gray-300',
+              ]"
               @click="selectColor(color)"
             />
           </div>
         </div>
 
         <!-- Quantity Selector -->
-        <div class="flex items-center gap-6">
-          <h3 class="text-sm font-medium text-gray-700">QUANTITY</h3>
-          <div class="flex items-center gap-3">
+        <div class="flex items-center gap-2">
+          <h3 class="font-medium dark:text-gray-300">QUANTITY :</h3>
+          <div class="dark:text-gray-500">
             {{ selectedColor?.quantity }}
           </div>
         </div>
 
         <!-- Description -->
         <div class="space-y-4">
-          <h3 class="text-sm font-medium text-gray-700">DESCRIPTION</h3>
+          <h3 class="font-medium text-gray-700 dark:text-gray-300">
+            DESCRIPTION
+          </h3>
           <p class="max-w-prose leading-relaxed text-gray-600">
             {{ product.description }}
           </p>
@@ -148,12 +133,14 @@ function getImageIndex() {
 
         <!-- Specifications -->
         <div class="space-y-6">
-          <h3 class="text-sm font-medium text-gray-700">SPECIFICATIONS</h3>
+          <h3 class="dark:text-gray-2 00 font-medium text-gray-300">
+            SPECIFICATIONS
+          </h3>
           <div class="grid gap-4 sm:grid-cols-2">
             <div
               v-for="(section, title) in product.additionalInformation"
               :key="title"
-              class="rounded-xl bg-gray-50 p-6"
+              class="rounded-lg bg-gray-50 p-6"
             >
               <h4
                 class="mb-3 text-xs font-semibold tracking-wider text-gray-600 uppercase"
